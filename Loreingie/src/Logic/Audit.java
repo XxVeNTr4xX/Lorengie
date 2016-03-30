f0d57933f06ee549ef3c4e88f324056b674c236d
Estudiante <Estudiante@DESKTOP-F4BE28I>
2016-03-30 12:51:19 -0500

    First version

---
+++ Loreingie/src/Logic/Audit.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Logic;
+
+import java.util.Date;
+
+/**
+ *
+ * @author Estudiante
+ */
+public class Audit {
+    
+    private String idProcess;
+    private int operation;
+    private Date dateAudit;
+    private String username;
+    
+}
